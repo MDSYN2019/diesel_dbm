40da7d907bc960d968a768a869573a7324ab6eee
SYN-MD <sangyoung123@gmail.com>
2026-03-28 16:38:45 +0000

    Add dbt project for fresh_segments Postgres models

---
+++ dbt_fresh_segments/models/staging/stg_fresh_segments__interest_metrics.sql
@@ -0,0 +1,24 @@
+with source as (
+    select *
+    from {{ source('fresh_segments', 'interest_metrics') }}
+),
+
+cleaned as (
+    select
+        nullif(_month, 'NULL')::integer as month_number,
+        nullif(_year, 'NULL')::integer as year_number,
+        case
+            when month_year = 'NULL' then null
+            else to_date(month_year, 'MM-YYYY')
+        end as month_start_date,
+        nullif(interest_id, 'NULL')::integer as interest_id,
+        composition::numeric(10, 4) as composition,
+        index_value::numeric(10, 4) as index_value,
+        ranking::integer as ranking,
+        percentile_ranking::numeric(10, 4) as percentile_ranking
+    from source
+)
+
+select *
+from cleaned
+where interest_id is not null
